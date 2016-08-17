175e6b6efafe0f0988e11b52ac528e3ccaa999bc
Caleb Stevenson and Kiet Duong <kiet@>
2016-08-17 13:58:20 -0700

    add fourth spec: program adds ay to single letter words beginning with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 var pigLatin = function(preSentence) {
-  if (preSentence === "a", "e", "i", "o", "u") {
-    return true;
+  if (preSentence === "a" || preSentence === "e" || preSentence === "i" || preSentence === "o" || preSentence ===  "u") {
+    return preSentence + "ay";
   } else if (preSentence !== "a", "e", "i", "o", "u") {
     return false;
+
   } else {
     return false;
   }
